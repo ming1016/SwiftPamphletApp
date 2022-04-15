62176bfae10696940366c722803ab3ed359b4504
daiming <ming1016@foxmail.com>
2022-04-15 12:24:08 +0800

    update

--- SwiftPamphletApp/IntroView.swift
+++ SwiftPamphletApp/IntroView.swift
@@ -21,9 +21,12 @@ struct IntroView: View {
                 Text("一本活的开发手册")
                 Link("GitHub 地址", destination: URL(string: "https://github.com/KwaiAppTeam/SwiftPamphletApp")!)
             }
-            Text("版本4.4").font(.footnote)
+            Text("版本4.6").font(.footnote)
         }
         .frame(minWidth: SPC.detailMinWidth)
         .enableInjection()
     }
 }
+
+
+
